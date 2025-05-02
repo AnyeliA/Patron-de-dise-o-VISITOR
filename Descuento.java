5a2741ee482ec0b6315049845af1a3284a76e842
Isabela Gonzalez <159276237+BelaIsBelaG@users.noreply.github.com>
2025-05-02 03:28:08 +0000

    Clase Descuento.java

---
+++ Descuento.java
@@ -0,0 +1,9 @@
+class Descuento implements Visitante {
+    public double visitar(EntradaNormal normal) {
+        return normal.getPrecio() * 0.9; // 10% de descuento
+    }
+
+    public double visitar(EntradaVIP vip) {
+        return vip.getPrecio() * 0.8; // 20% de descuento
+    }
+}
